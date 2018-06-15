b7b69a8b440e7be8002e4268cc15eae3362be68f
Dmitry <dmitriizykov64@gmail.com>
2018-06-15 20:46:15 +0300

    Validation layers cleanup

--- graphics/helperfunctions.h
+++ graphics/helperfunctions.h
@@ -12,7 +12,7 @@ struct SwapChainSupportDetails {
     std::vector<vk::PresentModeKHR> presentModes;
 };
 
-const std::vector<const char*> validationLayers = { "VK_LAYER_LUNARG_standard_validation", "VK_LAYER_LUNARG_parameter_validation", "VK_LAYER_LUNARG_parameter_validation", "VK_LAYER_LUNARG_object_tracker", "VK_LAYER_GOOGLE_threading", "VK_LAYER_LUNARG_core_validation" };
+const std::vector<const char*> validationLayers = { "VK_LAYER_LUNARG_standard_validation" };
 
 struct QueueFamilyIndices {
     int graphicsFamily = -1;
